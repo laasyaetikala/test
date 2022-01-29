deb73f39055858e86f743b4988cf8c9680976cf9
Laasya Etikala <86407455+laasyaetikala@users.noreply.github.com>
2022-01-29 23:44:26 +0530

    Add files via upload

---
+++ flaskapp/data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'Article 1',
+            'body': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos doloremque optio suscipit! Adipisci at consequuntur blanditiis ad quibusdam! Est unde minima fuga dicta odio soluta reiciendis sequi incidunt quo earum.',
+            'author':'Laasya',
+            'date_created':'27-01-2022'
+        },
+
+        {
+            'id':2,
+            'title':'Article 2',
+            'body': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos doloremque optio suscipit! Adipisci at consequuntur blanditiis ad quibusdam! Est unde minima fuga dicta odio soluta reiciendis sequi incidunt quo earum.',
+            'author':'Tejaswi',
+            'date_created':'24-01-2022'
+        },
+
+        {
+            'id':3,
+            'title':'Article 3',
+            'body': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos doloremque optio suscipit! Adipisci at consequuntur blanditiis ad quibusdam! Est unde minima fuga dicta odio soluta reiciendis sequi incidunt quo earum.',
+            'author':'Bahula',
+            'date_created':'25-01-2022'
+        }
+    ]
+    return articles
